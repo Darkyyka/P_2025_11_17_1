168411e8a8735b4646df5c786d623599628d52c5
info <info@205PC11>
2025-11-17 14:00:59 +0100

    VEGSOOOOOOOOOOO!!!

--- P_2025_11_17_'/Program.cs
+++ P_2025_11_17_'/Program.cs
@@ -37,24 +37,34 @@ namespace P_2025_11_17_2
 
             double d = b * b - 4 * a * c;
 
-            if (a == 0)
+            try
             {
-                Console.WriteLine("Ez nem másodfokú egyenlet (a nem lehet 0).");
+                if (a <= 0)
+                {
+                    throw new Exception("A paraméter nem lehet 0 vagy negatív!");
+                }
+
+                double d = b * b - 4 * a * c;
+
+                if (d > 0)
+                {
+                    double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                    double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                    Console.WriteLine($"Két valós gyök: x1 = {x1}, x2 = {x2}");
+                }
+                else if (d == 0)
+                {
+                    double x = -b / (2 * a);
+                    Console.WriteLine($"Egy valós gyök: x = {x}");
+                }
+                else
+                {
+                    throw new Exception("A diszkrimináns negatív, nincs valós gyök!");
+                }
             }
-            else if (d > 0)
+            catch (Exception ex)
             {
-                double x1 = (-b + Math.Sqrt(d)) / (2 * a);
-                double x2 = (-b - Math.Sqrt(d)) / (2 * a);
-                Console.WriteLine($"Két valós gyök: x1 = {x1}, x2 = {x2}");
-            }
-            else if (d == 0)
-            {
-                double x = -b / (2 * a);
-                Console.WriteLine($"Egy valós gyök: x = {x}");
-            }
-            else
-            {
-                Console.WriteLine("Nincs valós gyök!");
+                Console.WriteLine("Hiba történt: " + ex.Message);
             }
 
             Console.ReadKey();
